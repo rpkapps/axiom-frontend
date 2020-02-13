0abacb66dc4ae60128a38cf2b049d4fbb5d51185
Rus Kolesnik <rpkolesnikg@gmail.com>
2020-02-12 22:11:32 -0600

    Add angular material

--- apps/analytics/src/app/app.module.ts
+++ apps/analytics/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    MatButtonModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
