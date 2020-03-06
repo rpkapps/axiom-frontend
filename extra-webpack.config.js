bdd2aed7438ad0a20cbf5744e3de1de82b4b14d3
Rus Kolesnik <rpkolesnikg@gmail.com>
2020-03-05 21:18:02 -0600

    Still some issues with the theme plugin (disable theming for now)

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,19 +1,20 @@
 const ThemeLessPlugin = require('./libs/ui/src/styles/plugins/theme.js');
 
 module.exports = (config, options) => {
-  config.module.rules.push({
-    test: /\.less$/,
-    use: [
-      {
-        loader: 'less-loader',
-        options: {
-          sourceMap: options.sourceMap,
-          javascriptEnabled: true,
-          paths: (options.stylePreprocessorOptions &&  options.stylePreprocessorOptions.includePaths) || [],
-          plugins:[ThemeLessPlugin]
-        }
-      }
-    ]
-  });
+  // TODO RK: enable when the theme plugin is fixed
+  // config.module.rules.push({
+  //   test: /\.less$/,
+  //   use: [
+  //     {
+  //       loader: 'less-loader',
+  //       options: {
+  //         sourceMap: options.sourceMap,
+  //         javascriptEnabled: true,
+  //         paths: (options.stylePreprocessorOptions &&  options.stylePreprocessorOptions.includePaths) || [],
+  //         plugins:[ThemeLessPlugin]
+  //       }
+  //     }
+  //   ]
+  // });
   return config;
 };
